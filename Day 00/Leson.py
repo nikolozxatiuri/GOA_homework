88b7a6d13bc66b464ca3bd14ee72e3933c5c60d8
nikolozxatiuri <kaxamari099@gmail.com>
2024-05-18 06:21:34 -0700

    first commit

---
+++ Day 00/Leson.py
@@ -0,0 +1,45 @@
+from  turtle import *
+
+#we want to paint a house
+
+#step 1.
+sped(30)
+width(7)
+color("purple")
+forward(200)
+left(90)       
+        
+forward(200)       
+left(90)       
+                
+forward(200)       
+left(90)        
+
+forward(200)
+left(90)
+
+
+exitonclick()
+
+forward(70)
+color("yellow")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)        
+forward(120)        
+        
+penup()       
+goto(200,200)        
+pendown()      
+        
+color("red")       
+begin-fill()    
+forward(200)                
+left(120)
+foward(200)
+
+end-fill()
+
+exitonclik()
